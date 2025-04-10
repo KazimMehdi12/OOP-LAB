569dded0f55fc07211e7ea9264fe703757a98d33
KazimMehdi12 <kmsimair@gmail.com>
2025-04-10 19:33:47 +0500

    Create Q3.cpp

---
+++ LAB 08/Q3.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Vector2D{
+	private:
+		float x, y;
+
+	public:
+		Vector2D(float Inp1 = 0, float Inp2 = 0){
+			x = Inp1;
+			y = Inp2;
+		}
+
+		Vector2D operator+(const Vector2D& v) 
+		const{
+			return Vector2D(x+v.x, y+v.y);
+		}
+
+		Vector2D operator-(const Vector2D& v) 
+		const{
+			return Vector2D(x-v.x, y-v.y);
+		}
+
+		Vector2D operator*(float scalar) 
+		const{
+			return Vector2D(x*scalar, y*scalar);
+		}
+
+		friend float dotProduct(const Vector2D& v1, const Vector2D& v2);
+		friend ostream& operator<<(ostream& out, const Vector2D& v);
+};
+
+  float dotProduct(const Vector2D& v1, const Vector2D& v2){
+      return (v1.x*v2.x + v1.y*v2.y);
+}
+
+  ostream& operator<<(ostream& out, const Vector2D& v){
+  	  out<<"("<<v.x<<"- "<<v.y<<")";
+	  return out;
+}
+
+int main(){
+	Vector2D v1(3, 4);
+	Vector2D v2(1, 2);
+	
+	Vector2D sum = v1+v2;
+	Vector2D diff = v1-v2;
+	Vector2D scaled = v1*2;
+	float dot = dotProduct(v1, v2);
+
+	cout<<"v1: "<<v1<<endl;
+	cout<<"v2 = "<<v2<<endl;
+	cout<<"v1+v2 = "<<sum<<endl;
+	cout<<"v1-v2 = "<<diff<<endl;
+	cout<<"v1*2 = "<<scaled<<endl;
+	cout<<"Dot product of v1 and v2 = "<<dot<<endl;
+}
