9e7e6bee3fb70cfed2784b944e5150edbc5a67e3
KazimMehdi12 <kmsimair@gmail.com>
2025-04-18 10:04:17 +0500

    Create Q1.cpp

---
+++ LAB 09/Q1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Patient{
+protected:
+    string name;
+    string id;
+
+public:
+    Patient(string n, string i){
+        name = n;
+        id = i;
+    }
+
+    virtual void displayTreatment() = 0;
+    virtual void calculateCost() = 0;
+};
+
+class InPatient : public Patient{
+public:
+    InPatient(string n, string i) : Patient(n, i){}
+
+    void displayTreatment(){
+        cout<<"InPatient: "<<name<<endl;
+    }
+
+    void calculateCost(){
+        cout << "Cost: 4000" << endl;
+    }
+};
+
+class OutPatient : public Patient {
+public:
+    OutPatient(string n, string i) : Patient(n, i){}
+
+    void displayTreatment(){
+        cout << "OutPatient: " << name << endl;
+    }
+
+    void calculateCost(){
+        cout << "Cost: 5000" << endl;
+    }
+};
+
+int main() {
+    Patient* p1 = new InPatient("Kazim", "A1");
+    Patient* p2 = new OutPatient("Ali", "A2");
+
+    p1->displayTreatment();
+    p1->calculateCost();
+
+    p2->displayTreatment();
+    p2->calculateCost();
+
+    delete p1;
+    delete p2;
+}
