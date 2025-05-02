d2ae594b2ec44f66f59d86fbe922f434c2649a98
KazimMehdi12 <kmsimair@gmail.com>
2025-05-02 19:26:43 +0500

    Create Q1.cpp

---
+++ LAB 10/Q1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+class Vehicles{
+		ifstream file;
+		string line;
+	public:
+		void readFile(){
+			file.open("vehicles.txt");
+
+			if(!file.is_open()){
+				cout<<"Error: File not opened"<<endl;
+				return;
+			}
+
+			while(getline(file, line)){
+				if(line[0] == '#' || line.empty()){
+					continue;
+				}
+				cout<<"Line: "<<line<<endl;
+			}
+
+			file.close();
+		}
+};
+
+int main(){
+	Vehicles v1;
+	v1.readFile();
+	return 0;
+	
+}
